3bbb918943c03fb6b041bbbbc71d75799285f3de
Jan Machacek <janm@cakesolutions.net>
2011-11-16 10:22:46 +0000

    Akka demo app

---
+++ scalad.akka-example/src/main/scala/scalad/example/User.java
@@ -0,0 +1,53 @@
+package scalad.example;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+/**
+ * @author janmachacek
+ */
+@Entity
+public class User {
+	@Id @GeneratedValue
+	private Long id;
+	@Version
+	private int version;
+	private String username;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("User");
+		sb.append("{id=").append(id);
+		sb.append(", version=").append(version);
+		sb.append(", username='").append(username).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+}
---
+++ scalad.akka-example/src/main/scala/scalad/example/jpa/Main.scala
@@ -0,0 +1,53 @@
+package scalad.example.hibernate
+
+import scalad.example.User
+import javax.persistence.{EntityManager, Persistence}
+import scalad.transaction.Transactions
+
+/**
+ * @author janmachacek
+ */
+object Main {
+
+  import scalaz.IterV._
+  import scalaz._
+  import Scalaz._
+  import scalad._
+  import Scalad._
+
+  def main(args: Array[String]) {
+    val entityManagerFactory = Persistence.createEntityManagerFactory("org.cakesolutions.scaladata.core.example")
+    val entityManager = entityManagerFactory.createEntityManager()
+    new Worker(entityManager).work()
+  }
+  
+  class Worker(entityManager: EntityManager) extends JPA(entityManager) with Transactions {
+
+    def work() {
+      for (i <- 0 to 20) {
+        val u = new User()
+        u.setUsername("a" + i)
+        transactionally(getPlatformTransactionManager) { persist(u) }
+      }
+
+      val users = select(list[User])
+      println(users)
+
+//      val f = sel(list[User])
+//      val b = f | ("username" like "a%") |
+//      println(b)
+
+      val m1 = head[User] >>= (u => head map (u2 => (u <|*|> u2)))
+      val firstTwo = selector(m1)
+      println(firstTwo("username" like "a2%"))
+//
+//      select(one[User])
+//      selectThat(one[User])("username" like "B")
+//
+//      val usersWhose = selectThat(head[User])("username" like "a1%")
+//      println(usersWhose)
+    }
+
+  }
+
+}
