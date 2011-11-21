db6e606b32ecb5eb083a368898577137abc7ab07
Jan Machacek <janmachacek@Jans-iMac.local>
2011-11-21 16:27:08 +0000

    Hibernate 4

--- scalad.core/src/test/scala/scalad/QuerySpec.scala
+++ scalad.core/src/test/scala/scalad/QuerySpec.scala
@@ -5,10 +5,8 @@ import org.specs2.mutable.Specification
 /**
  * @author janmachacek
  */
-class QuerySpec extends Specification {
+class QuerySpec extends Specification with OrmLike {
   import Scalad._
-
-  implicit def toQuery(restriction: Restriction) = new Query(restriction, Nil, Nil, None)
 
   "guard" in {
     "must reduce to nothing when `false`"       ! restrictionMatch((id ＝ "a" when false), Skip())
