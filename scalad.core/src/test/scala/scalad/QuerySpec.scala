b3359c11a41f55f8684ddbb34705b72ab9a30e67
Jan Machacek <janmachacek@Jans-iMac.local>
2011-11-21 11:57:38 +0000

    Formatting

--- scalad.core/src/test/scala/scalad/QuerySpec.scala
+++ scalad.core/src/test/scala/scalad/QuerySpec.scala
@@ -9,33 +9,33 @@ class QuerySpec extends Specification {
   import Scalad._
 
   "guard" in {
-    "must reduce to nothing when `false`"     ! restrictionMatch((id ＝ "a" when false), Nothing())
+    "must reduce to nothing when `false`"       ! restrictionMatch((id ＝ "a" when false), Nothing())
 
-    "must reduce to LHS in || when `false`"   ! restrictionMatch(((id ＝ 5) || ("foo" ＝ "a" when false)), (id ＝ 5))
-    "must reduce to RHS in || when `false`"   ! restrictionMatch((("foo" ＝ "a" when false) || (id ＝ 5)), (id ＝ 5))
+    "must reduce to LHS in || when `false`"     ! restrictionMatch(((id ＝ 5) || ("foo" ＝ "a" when false)), (id ＝ 5))
+    "must reduce to RHS in || when `false`"     ! restrictionMatch((("foo" ＝ "a" when false) || (id ＝ 5)), (id ＝ 5))
 
-    "must reduce to LHS in && when `false`"   ! restrictionMatch(((id ＝ 5) && ("foo" ＝ "a" when false)), (id ＝ 5))
-    "must reduce to RHS in && when `false`"   ! restrictionMatch((("foo" ＝ "a" when false) && (id ＝ 5)), (id ＝ 5))
+    "must reduce to LHS in && when `false`"     ! restrictionMatch(((id ＝ 5) && ("foo" ＝ "a" when false)), (id ＝ 5))
+    "must reduce to RHS in && when `false`"     ! restrictionMatch((("foo" ＝ "a" when false) && (id ＝ 5)), (id ＝ 5))
   }
 
   "tautology in disjunction" in {
-    "tautology `foo=a || foo!=a`"         ! restrictionMatch(((id ＝ "a") || (id !＝ "a")), Tautology())
-    "tautology `foo=a || foo!=a`"         ! restrictionMatch(((id ＝ "a") || (id !＝ "a")), Tautology())
+    "tautology `foo=a || foo!=a`"               ! restrictionMatch(((id ＝ "a") || (id !＝ "a")), Tautology())
+    "tautology `foo=a || foo!=a`"               ! restrictionMatch(((id ＝ "a") || (id !＝ "a")), Tautology())
   }
 
   "reduction in disjunction" in {
-    "must reduce to LHS"                  ! restrictionMatch(((id ＝ "a") || ((id !＝ "a") && (id ＝ "a"))), (id ＝ "a"))
-    "must reduce to RHS"                  ! restrictionMatch((((id ＝ "a") || (id ＝ "a")) || (id ＝ "a")), (id ＝ "a"))
+    "must reduce to LHS"                        ! restrictionMatch(((id ＝ "a") || ((id !＝ "a") && (id ＝ "a"))), (id ＝ "a"))
+    "must reduce to RHS"                        ! restrictionMatch((((id ＝ "a") || (id ＝ "a")) || (id ＝ "a")), (id ＝ "a"))
   }
 
   "contradiction in conjunction" in {
-    "must contradict `foo=a && foo!=a`"    ! restrictionMatch((("foo" ＝ "a") && ("foo" !＝ "a")), Contradiction())
-    "must contradict `foo=a && foo=b`"     ! restrictionMatch((("foo" ＝ "a") && ("foo" ＝ "B")), Contradiction())
+    "must contradict `foo=a && foo!=a`"         ! restrictionMatch((("foo" ＝ "a") && ("foo" !＝ "a")), Contradiction())
+    "must contradict `foo=a && foo=b`"          ! restrictionMatch((("foo" ＝ "a") && ("foo" ＝ "B")), Contradiction())
 
-    "contradict `id=a && (id=a && id!=a)`" ! restrictionMatch(((id ＝ "a") && ((id !＝ "a") && (id ＝ "a"))), Contradiction())
-    "contradict `(id=a && id!=a) && id=a`" ! restrictionMatch((((id ＝ "a") && (id !＝ "a")) && (id ＝ "a")), Contradiction())
+    "contradict `id=a && (id=a && id!=a)`"      ! restrictionMatch(((id ＝ "a") && ((id !＝ "a") && (id ＝ "a"))), Contradiction())
+    "contradict `(id=a && id!=a) && id=a`"      ! restrictionMatch((((id ＝ "a") && (id !＝ "a")) && (id ＝ "a")), Contradiction())
 
-    "contradict `id=a && id is null`"      ! restrictionMatch((id ＝ "a") && (id isNull), Contradiction())
+    "contradict `id=a && id is null`"           ! restrictionMatch((id ＝ "a") && (id isNull), Contradiction())
   }
 
   "duplicate terms" in {
