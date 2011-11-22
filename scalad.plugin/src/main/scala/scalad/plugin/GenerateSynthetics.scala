728ee719e8eb660f0d7d27ba77ce5364302bb3e8
Jan Machacek <janmachacek@Jans-iMac.local>
2011-11-22 13:46:49 +0000

    Synthetic generator

--- scalad.plugin/src/main/scala/scalad/plugin/GenerateSynthetics.scala
+++ scalad.plugin/src/main/scala/scalad/plugin/GenerateSynthetics.scala
@@ -17,9 +17,32 @@ class GenerateSynthetics(plugin: ScaladPlugin, val global: Global) extends Plugi
   protected def newTransformer(unit: CompilationUnit) = new ScaladTransformer(unit)
 
   class ScaladTransformer(unit: CompilationUnit) extends TypingTransformer(unit) {
+    import CODE._
+
+    def shouldGenerate(sym: Symbol) = sym.isCaseClass && sym.annotations.exists(_.atp.typeSymbol == annotationClass)
+
+    def shouldGenerateForMember(tree: Tree) = {
+      val sym = tree.symbol
+      sym.isCaseAccessor && sym.isParamAccessor && sym.isMethod
+    }
+
+    override def transform(tree: Tree): Tree = {
+      val newTree = tree match {
+        case cd @ ClassDef(_, _, _, _) if shouldGenerate(cd.symbol) =>
+          //          log("found case class. classdef.symbol.tpe: " + cd.symbol.tpe)
+          //          log("case class impl: classdef.tpe"+ cd.tpe)
+          caseClasses +=  cd.symbol -> cd
+          cd
+        case _ => tree
+      }
+      super.transform(newTree)
+    }
 
   }
 
+
+  val caseClasses = collection.mutable.HashMap[global.Symbol, ClassDef]()
+  val annotationClass = definitions.getClass("scalad.annotation.selectable")
   val runsAfter = List("typer")
   val phaseName = "generatesynthetics"
 }
