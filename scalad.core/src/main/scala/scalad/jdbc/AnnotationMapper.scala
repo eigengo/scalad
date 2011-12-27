de70796bae9d32acda635e6f09f84f428b64c39d
Jan Machacek <janmachacek@Jans-MacBook-Pro.local>
2011-12-27 17:13:08 +0000

    Test

--- scalad.core/src/main/scala/scalad/jdbc/AnnotationMapper.scala
+++ scalad.core/src/main/scala/scalad/jdbc/AnnotationMapper.scala
@@ -6,7 +6,7 @@ import java.sql.ResultSet
  * @author janmachacek
  */
 trait AnnotationMapper {
-  
+
   def mapper[T](implicit evidence: ClassManifest[T]) = {rs: ResultSet =>
       evidence.erasure.newInstance().asInstanceOf[T]
   }
