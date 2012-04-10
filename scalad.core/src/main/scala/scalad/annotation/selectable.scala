b192d12a572d97559bd53820a830f48d4b71cff8
Jan Machacek <jan.machacek@gmail.com>
2012-04-10 11:10:34 +0100

    example

--- scalad.core/src/main/scala/scalad/annotation/selectable.scala
+++ scalad.core/src/main/scala/scalad/annotation/selectable.scala
@@ -3,4 +3,4 @@ package scalad.annotation
 /**
  * @author janmachacek
  */
-final class selectable extends scala.annotation.StaticAnnotation
+final class selectable extends scala.annotation.StaticAnnotation 
