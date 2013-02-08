04e1b874e4bd78fc8daeb0cd2fcdcbeeab578ce9
Jan Machacek <janm@cakesolutions.net>
2013-02-08 14:10:33 +0000

    This is it

--- src/test/scala/org/cakesolutions/scalad/RestrictionsSpec.scala
+++ src/test/scala/org/cakesolutions/scalad/RestrictionsSpec.scala
@@ -1,34 +1,46 @@
 package org.cakesolutions.scalad
 
-import mongo._
 import mongo.LongEntity
 import org.specs2.mutable.Specification
-import com.mongodb.DBObject
 
-class RestrictionsSpec extends Specification with Restrictions with StringRestrictionsPaths with SprayJsonNativeRestrictions
-  with LongEntityPersistence {
+class RestrictionsSpec extends Specification with Restrictions with StringRestrictionsPaths with SpecNativeRestrictions {
+
+  noopNativeRestrictionMarshaller[Any]
 
   "trivial query" in {
     val le = LongEntity(1, "foo")
-    val query: DBObject = "someobj" equalTo le
-    println(query)
-    success
-    //query.r mustEqual EqualsRestriction("username", "foo")
+    val query: SpecNativeRestriction = "someobj" equalTo le
+    query.r mustEqual EqualsRestriction("someobj", le)
   }
 
-//  "conjunctions and disjunctions" should {
-//
-//    "combine simple restrictions" in {
-//      val query: SpecNativeRestriction = ("username" equalTo "foo") && ("password" equalTo "merde embulante")
-//      query.r mustEqual ConjunctionRestriction(EqualsRestriction("username", "foo"),
-//                                             EqualsRestriction("password", "merde embulante"))
-//    }
-//
-//    "simplify well" in {
-//      val query: SpecNativeRestriction = ("username" equalTo "foo") && ("username" notEqualTo "foo")
-//      query.r mustEqual ContradictionRestriction
-//    }
-//
-//  }
+  "conjunctions and disjunctions" should {
+
+    "combine simple restrictions" in {
+      val query: SpecNativeRestriction = ("username" equalTo "foo") && ("password" equalTo "merde embulante")
+      query.r mustEqual ConjunctionRestriction(EqualsRestriction("username", "foo"),
+                                             EqualsRestriction("password", "merde embulante"))
+    }
+
+    "simplify well" in {
+      val query: SpecNativeRestriction = ("username" equalTo "foo") && ("username" notEqualTo "foo")
+      query.r mustEqual ContradictionRestriction
+    }
+
+  }
 
 }
+
+case class SpecNativeRestriction(r: Restriction)
+
+trait SpecNativeRestrictions extends NativeRestrictions {
+
+  type NativeRestriction = SpecNativeRestriction
+
+  def convertToNative(restriction: Restriction) = SpecNativeRestriction(restriction)
+
+  implicit def noopNativeRestrictionMarshaller[A] = new NativeRestrictionsMarshaller[A] {
+    type NativeRestrictionValue = A
+    def marshal(value: A) = value
+  }
+}
+
