2ad512983cc069f79fa24197b5fbc183b9ae5b85
Sam Halliday <sam.halliday@gmail.com>
2013-03-19 18:23:58 +0000

    clean up the spray dsl tests

--- src/test/scala/org/cakesolutions/scalad/mongo/sprayjson/JsonDslSpec.scala
+++ src/test/scala/org/cakesolutions/scalad/mongo/sprayjson/JsonDslSpec.scala
@@ -12,46 +12,32 @@ class JsonDslSpec extends Specification with DefaultJsonProtocol {
   "the JSON DSL should" should {
 
     "produce a valid JsValue out of a simple key/String mapping" in {
-      val res = {"foo" :> "bar"}
-      val expected = Map("foo" -> "bar").toJson
-      res.compactPrint mustEqual(expected.compactPrint)
+      {"foo" :> "bar"} === JsonParser("""{"foo":"bar"}""")
     }
 
     "produce a valid JsValue out of a simple key/Int mapping" in {
-      val res = {"foo" :> 10}
-      val expected = Map("foo" -> 10).toJson
-      res.compactPrint mustEqual(expected.compactPrint)
+      {"foo" :> 10} === JsonParser("""{"foo":10}""")
     }
 
     "produce a valid JsValue out of a simple key/Boolean mapping" in {
-      val res = {"foo" :> true}
-      val expected = Map("foo" -> true).toJson
-      res.compactPrint mustEqual(expected.compactPrint)
+      {"foo" :> true} === JsonParser("""{"foo":true}""")
     }
 
     "produce a valid JsValue out of a nested mapping" in {
-      val res = "foo" :> {"bar" :> 10}
-      val expected = Map("foo" -> Map("bar" -> 10)).toJson
-      res.compactPrint mustEqual(expected.compactPrint)
+      "foo" :> {"bar" :> 10} === JsonParser("""{"foo": {"bar":10}}""")
     }
 
     "allow monoid-like mappending of objects" in {
-      val a1 = {"foo" :> {"bar" :> 10}} <> {"age" :> 45}
-      val expected = a1.compactPrint
-      expected mustEqual("""{"foo":{"bar":10},"age":45}""")
+     ({"foo" :> {"bar" :> 10}} <> {"age" :> 45}) === JsonParser("""{"foo":{"bar":10},"age":45}""")
     }
 
     "correctly handle JSON arrays" in {
-      val a1 = $(1,2,3)
-      val expected = a1.compactPrint
-      expected mustEqual("[1,2,3]")
+      $(1,2,3) === JsonParser("[1,2,3]")
     }
 
     "Correctly handle combination of nested object and arrays" in {
       val a1 = {"lorem" :> $("ipsum", "lorem ipsum")}
-      val a2 = {"foo" :> 10} ~ {"bar" :> a1}
-      val expected = a2.compactPrint
-      expected mustEqual("""{"foo":10,"bar":{"lorem":["ipsum","lorem ipsum"]}}""")
+      {"foo" :> 10} ~ {"bar" :> a1} === JsonParser("""{"foo":10,"bar":{"lorem":["ipsum","lorem ipsum"]}}""")
     }
 
   }
